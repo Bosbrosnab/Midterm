4d2af594487f6fac287c680de22c603cd4dc1a53
Bosbrosnab <abowes@unomaha.edu>
2024-10-23 16:35:24 -0500

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,20 @@
 #Midterm Exam
 #Problem2.py
-#Name:
-#Date:
+#Name: Alex Bowes
+#Date: 10/23/24
+
+def countvows(instr):
+    
+    vows = "AaEeIiOoUu"
+    vowcount = 0
+    
+    for char in instr:
+        for vow in vows:
+            if char == vow:
+                vowcount += 1
+                break
+    return vowcount
+
+userin = input("Enter a word/sentence: ")
+result = countvows(userin)
+print("The number of vowels in word/sentence is:", result)
